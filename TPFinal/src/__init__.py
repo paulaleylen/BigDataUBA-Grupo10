495898e4487cf6bd85e542ad12d2fd62f9105ac3
Juanpintoselso33 <juanpintoselso33@gmail.com>
2025-11-04 21:07:28 -0300

    fixed missing exports in modules

--- TPFinal/src/__init__.py
+++ TPFinal/src/__init__.py
@@ -1,2 +1,28 @@
-# commodities_project - Base de Commodities UBA-FCE
+"""
+commodities_project - Base de Commodities UBA-FCE
+
+Análisis de commodities y predictores macroeconómicos para el Trabajo Final
+de la materia Taller de Programación - Universidad de Buenos Aires, FCE.
+
+Estructura del paquete:
+- data: Descarga y procesamiento de datos
+- visualization: Gráficos con estilo institucional
+- features: Feature engineering (futuro)
+- models: Modelos predictivos (futuro)
+- utils: Utilidades generales
+"""
+
 __version__ = '0.1.0'
+__author__ = 'Grupo JLP'
+__email__ = 'grupo@uba.edu.ar'
+
+from . import data
+from . import visualization
+from . import config
+
+__all__ = [
+    'data',
+    'visualization',
+    'config',
+    '__version__',
+]
--- TPFinal/src/data/__init__.py
+++ TPFinal/src/data/__init__.py
@@ -1,1 +1,15 @@
-# Data download and processing modules
+"""
+Data download and processing modules
+
+This package contains modules for downloading and processing commodity and predictor data.
+"""
+
+from . import download_commodities
+from . import download_predictors
+from . import process
+
+__all__ = [
+    'download_commodities',
+    'download_predictors',
+    'process',
+]
