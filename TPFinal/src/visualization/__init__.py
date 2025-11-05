0b535acc70d97ce6050b4d8bcd5edb80502bbb32
Juanpintoselso33 <juanpintoselso33@gmail.com>
2025-11-04 21:07:28 -0300

    fixed missing exports in modules

--- TPFinal/src/visualization/__init__.py
+++ TPFinal/src/visualization/__init__.py
@@ -1,1 +1,24 @@
-# Visualization utilities
+"""
+Visualization utilities
+
+This package contains modules for creating consistent, styled visualizations
+following institutional UBA-FCE guidelines.
+"""
+
+from . import estilo_graficos
+
+# Import commonly used functions directly
+from .estilo_graficos import (
+    configurar_estilo_grafico,
+    formatear_ejes,
+    forzar_y_cero,
+    UBA_FCE_COLORS,
+)
+
+__all__ = [
+    'estilo_graficos',
+    'configurar_estilo_grafico',
+    'formatear_ejes',
+    'forzar_y_cero',
+    'UBA_FCE_COLORS',
+]
