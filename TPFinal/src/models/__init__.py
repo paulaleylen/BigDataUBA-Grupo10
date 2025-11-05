a4394382dac8e223977e4005498ec2809166f16e
Juanpintoselso33 <juanpintoselso33@gmail.com>
2025-11-04 21:07:28 -0300

    fixed missing exports in modules

--- TPFinal/src/models/__init__.py
+++ TPFinal/src/models/__init__.py
@@ -1,1 +1,8 @@
-# Machine learning models
+"""
+Machine learning models
+
+This package will contain modules for training and evaluating
+predictive models for commodity prices.
+"""
+
+__all__ = []
